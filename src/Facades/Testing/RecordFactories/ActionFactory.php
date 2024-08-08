41c12bb1291ae53dac5adee34c5ac1ad8d065bed
Bruno Görß <bruno.goerss@inno-brain.de>
2024-08-08 16:31:23 +0200

    fix: add correct id and type for action faactory

--- src/Facades/Testing/RecordFactories/ActionFactory.php
+++ src/Facades/Testing/RecordFactories/ActionFactory.php
@@ -4,4 +4,8 @@ declare(strict_types=1);
 
 namespace Katalam\OnOfficeAdapter\Facades\Testing\RecordFactories;
 
-class ActionFactory extends BaseFactory {}
+class ActionFactory extends BaseFactory
+{
+    public int|string $id = '0';
+    public string $type = 'actionkind';
+}
