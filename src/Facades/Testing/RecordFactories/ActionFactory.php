c9d3f5f417c75867a2f70ec2a92440e8501962c7
Katalam <Katalam@users.noreply.github.com>
2024-08-08 14:31:49 +0000

    chore: code style issues

--- src/Facades/Testing/RecordFactories/ActionFactory.php
+++ src/Facades/Testing/RecordFactories/ActionFactory.php
@@ -7,5 +7,6 @@ namespace Katalam\OnOfficeAdapter\Facades\Testing\RecordFactories;
 class ActionFactory extends BaseFactory
 {
     public int|string $id = '0';
+
     public string $type = 'actionkind';
 }
