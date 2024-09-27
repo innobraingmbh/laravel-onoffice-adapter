c2e22efe7f51c42d1422a0cf9f3d40a1f8f569b5
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-09-27 11:24:55 +0200

    feat: change default block size to a more performant value
    - increases block size, as testing shows significant speedup in uploads when using larger blocks

--- src/Query/Concerns/UploadInBlocks.php
+++ src/Query/Concerns/UploadInBlocks.php
@@ -8,7 +8,12 @@ trait UploadInBlocks
 {
     private int $uploadInBlocks = 0;
 
-    public function uploadInBlocks(int $blocks = 5120): self
+    /**
+     * Uploads the file in blocks of the given size.
+     *
+     * @param  int  $blocks  The size of the blocks, measured in UTF-8 characters.
+     */
+    public function uploadInBlocks(int $blocks = 20480): self
     {
         $this->uploadInBlocks = $blocks;
 
