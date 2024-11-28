0f4637ec0d7e2fe6f0b31235193df1737a55d925
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-11-28 22:35:11 +0100

    feat: add handy where helpers

--- src/Query/Concerns/NonFilterable.php
+++ src/Query/Concerns/NonFilterable.php
@@ -10,4 +10,34 @@ trait NonFilterable
     {
         return $this;
     }
+
+    public function whereNot(string $column, mixed $value): static
+    {
+        return $this;
+    }
+
+    public function whereIn(string $column, array $values): static
+    {
+        return $this;
+    }
+
+    public function whereNotIn(string $column, array $values): static
+    {
+        return $this;
+    }
+
+    public function whereBetween(string $column, int|string $start, int|string $end): static
+    {
+        return $this;
+    }
+
+    public function whereLike(string $column, string $value): static
+    {
+        return $this;
+    }
+
+    public function whereNotLike(string $column, string $value): static
+    {
+        return $this;
+    }
 }
