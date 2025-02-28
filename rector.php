56b4ae244f1b61331624cca680107258be666162
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-02-28 14:46:57 +0100

    chore: upgrade rector config

--- rector.php
+++ rector.php
@@ -8,28 +8,31 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Rector\Expr\AppEnvironmentComparisonToParameterRector;
 use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
-use RectorLaravel\Set\LaravelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/config',
         __DIR__.'/resources',
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->rule(RemoveDumpDataDeadCodeRector::class);
-    $rectorConfig->rule(AppEnvironmentComparisonToParameterRector::class);
-
-    $rectorConfig->sets([
+    ])
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+        RemoveDumpDataDeadCodeRector::class,
+        AppEnvironmentComparisonToParameterRector::class,
+    ])
+    ->withSets([
         LevelSetList::UP_TO_PHP_82,
         SetList::DEAD_CODE,
         SetList::CODE_QUALITY,
         SetList::EARLY_RETURN,
-        LaravelSetList::LARAVEL_110,
-        LaravelSetList::LARAVEL_IF_HELPERS,
-        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        RectorLaravel\Set\LaravelLevelSetList::UP_TO_LARAVEL_110,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_CODE_QUALITY,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_IF_HELPERS,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_COLLECTION,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        RectorLaravel\Set\LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
     ]);
-};
