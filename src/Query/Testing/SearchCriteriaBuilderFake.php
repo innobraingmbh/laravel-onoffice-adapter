663470750e154e4fe3735fb37a6a833cf5581574
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-04-14 15:57:05 +0200

    fix: allow all calls in fake

--- src/Query/Testing/SearchCriteriaBuilderFake.php
+++ src/Query/Testing/SearchCriteriaBuilderFake.php
@@ -4,4 +4,9 @@ declare(strict_types=1);
 
 namespace Katalam\OnOfficeAdapter\Query\Testing;
 
-class SearchCriteriaBuilderFake extends BaseFake {}
+class SearchCriteriaBuilderFake extends BaseFake {
+    public function __call(string $name, array $arguments): static
+    {
+        return $this;
+    }
+}
