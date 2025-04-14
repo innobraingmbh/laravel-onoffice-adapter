4bca83de0a07b8d50948920aa7e7b87cddc757eb
kauffinger <62616071+kauffinger@users.noreply.github.com>
2025-04-14 13:57:24 +0000

    chore: code style issues

--- src/Query/Testing/SearchCriteriaBuilderFake.php
+++ src/Query/Testing/SearchCriteriaBuilderFake.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Katalam\OnOfficeAdapter\Query\Testing;
 
-class SearchCriteriaBuilderFake extends BaseFake {
+class SearchCriteriaBuilderFake extends BaseFake
+{
     public function __call(string $name, array $arguments): static
     {
         return $this;
